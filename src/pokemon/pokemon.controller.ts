40283d2d8c173c09f03743634d984d238d4f3d8c
Paul Llanque <llanquepol@gmail.com>
2023-10-16 23:05:28 -0500

    [ADD] pagination added

--- src/pokemon/pokemon.controller.ts
+++ src/pokemon/pokemon.controller.ts
@@ -4,6 +4,7 @@ import {
   Post,
   Body,
   Patch,
+  Query,
   Param,
   Delete,
   HttpCode,
@@ -13,6 +14,7 @@ import { PokemonService } from './pokemon.service';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { ParseMongoIdPipe } from 'src/common/pipes/parse-mongo-id.pipe';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Controller('pokemon')
 export class PokemonController {
@@ -25,8 +27,8 @@ export class PokemonController {
   }
 
   @Get()
-  findAll() {
-    return this.pokemonService.findAll();
+  findAll(@Query() paginationDto: PaginationDto) {
+    return this.pokemonService.findAll(paginationDto);
   }
 
   @Get(':term')
