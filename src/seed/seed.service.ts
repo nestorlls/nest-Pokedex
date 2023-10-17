a17ea9ff434d3ae549369c07d8239273b44bd9d5
Paul Llanque <llanquepol@gmail.com>
2023-10-16 22:06:51 -0500

    [ADD] seed added

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,19 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+
 import axios, { AxiosInstance } from 'axios';
+import { Model } from 'mongoose';
+
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios;
+
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
   async executeSeed() {
+    await this.pokemonModel.deleteMany({});
+
     const { data } = await this.axios.get<PokeResponse>(
       'https://pokeapi.co/api/v2/pokemon?limit=650',
     );
 
-    return data.results.map(({ name, url }) => {
+    const pokemonToInsert: { name: string; no: number }[] = [];
+
+    data.results.forEach(({ name, url }) => {
       const segendedUrl = url.split('/');
       const no: number = +segendedUrl[segendedUrl.length - 2];
-      return { name, no };
+      pokemonToInsert.push({ name, no });
     });
+
+    await this.pokemonModel.insertMany(pokemonToInsert);
+    return 'Seed Executed';
   }
 }
