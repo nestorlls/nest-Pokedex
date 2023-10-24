ccecbc8ef36de59e76b1e51bb037e875ed9e4f33
Paul Llanque <llanquepol@gmail.com>
2023-10-23 20:41:46 -0500

    [ADD] environment validations

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,11 +8,13 @@ import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { ConfigModule } from '@nestjs/config';
 import { envConfig } from './config/app.config';
+import { JoinValidationSchema } from './config/joi.validation';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [envConfig],
+      validationSchema: JoinValidationSchema,
     }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
